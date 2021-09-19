402b2052540ba139fefcd05b20480ddb916022f3
PofesorAlejandro <alejandroaulanosa@gmail.com>
2021-09-19 09:24:10 +0200

    Superglobales

---
+++ superglobales/index.php
@@ -0,0 +1,37 @@
+<?php
+
+//VARIABLES SUPERGLOBALES O PREDEFINIDAS
+
+
+  //Array asociativo al cual contiene todas infomación de una petición GET
+ // al servidor  
+//  $_GET[''];
+
+ //Array asociativo al cual contiene todas infomación de una petición POST
+ // al servidor 
+//  $_POST[''];
+
+// Array asocitivo de las cookies lamacenadas en el pc del usuario
+//  $_COOKIE[''];
+
+ //Array asociativo al cual contiene todas infomación de una petición 
+ // al servidor  ( get o post ) y de las cookies
+//  $_REQUEST[''];
+
+ //Array asociativo de las variables de session
+//  $_SESSION[''];
+
+ //var_dump($_SERVER);
+
+ //echo $_SERVER['MYSQL_HOME'];
+
+
+ $nombre =$_GET['nombre'];
+
+ echo $nombre;
+
+
+
+
+
+
