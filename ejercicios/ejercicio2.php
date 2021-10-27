dbb66e61aaefc0d9cf28bdd160d1a6fd6fd308d4
PofesorAlejandro <alejandroaulanosa@gmail.com>
2021-10-27 11:46:05 +0200

    Ej23

--- ejercicios/ejercicio2.php
+++ ejercicios/ejercicio2.php
@@ -1,7 +1,7 @@
 <?php
 
 // Realiza un script en php que muestre en el navegador todos los núermos pares del 1 al 100.
-// Los núemeros se deben pintar dentro de un <h4></h4> y deben aparecer cada un o en un línea
+// Los números se deben pintar dentro de un <h4></h4> y deben aparecer cada un o en un línea
 
 
 for($i=0;$i<=100;$i++){
